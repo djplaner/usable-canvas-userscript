6fc6bb2d6f01645aea21ba78bda2fe41732c1dab
David Jones <davidthomjones@gmail.com>
2023-02-03 05:39:58 +1000

    Initial set up

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { configStore  } from "./stores";
+  import { CanvasDetails } from "./lib/CanvasDetails";
+
+  export let courseId: number;
+  export let editMode: boolean;
+  export let csrfToken: string;
+  export let currentPage: string;
+
+  $configStore = {
+    courseId: courseId,
+    editMode: editMode,
+    csrfToken: csrfToken,
+    currentPage: currentPage,
+  };
+
+</script>
+
+<p class="hello">Hello world, on the {$configStore.currentPage}</p>
+
+
+
+
+<style>
+  .hello {
+    background-color: black;
+    color: white;
+  }
+</style>
